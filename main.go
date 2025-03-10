c12acc208f1ac02ae400e76dbfc1364d1bb2f4ce
Azat Valiev <azat@valievventures.com>
2025-03-10 09:31:04 -0500

    reorganize

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path"
 )
 
 func main() {
@@ -14,40 +13,33 @@ func main() {
 }
 
 func Run() int {
-	tempfile, error := getTempFile()
+	playground, error := setupTempFiles()
+
+	// Make sure we clean up the playground
+	defer func() {
+		if err := playground.Destroy(); err != nil {
+			fmt.Fprintf(
+				os.Stderr,
+				"Failed to cleanup at %s\n%s\n",
+				playground.DirName,
+				error.Error(),
+			)
+		}
+	}()
+
 	if error != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", error.Error())
 		return 1
 	}
 
-	// Make sure we clean up the file
-	defer func() {
-		error := CleanupFiles(tempfile)
-		if error != nil {
-			fmt.Fprintf(os.Stderr, "Failed to cleanup at %s\n%s\n", tempfile.DirName, error.Error())
-		}
-	}()
-
-	fmt.Printf("Created temp file at %s\n", tempfile.File.Name())
-
-	err := SetupPlaygroundConfig(tempfile.DirName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-		return 1
-	}
-
-	fmt.Printf("Setup playground at %s\n", tempfile.DirName)
-
 	// Run TS Playground
 	{
-		err := OpenNeovim(tempfile)
-		if err != nil {
+		if err := OpenNeovim(playground); err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 			return 1
 		}
 
-		err = CommandLoop(tempfile)
-		if err != nil {
+		if err := CommandLoop(playground); err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 			return 1
 		}
@@ -63,7 +55,7 @@ const (
 	CommandEditor Command = 'e'
 )
 
-func CommandLoop(playgroundFiles *PlaygroundFiles) error {
+func CommandLoop(playground *Playground) error {
 	for {
 		fmt.Printf(
 			"Enter command (%s to run, %s for editor, ctrl-c to exit)\n"+
@@ -85,13 +77,13 @@ func CommandLoop(playgroundFiles *PlaygroundFiles) error {
 		switch Command(cmd[0]) {
 		case CommandRun:
 			{
-				out := RunScript(playgroundFiles)
+				out := RunScript(playground)
 				fmt.Printf("\n%s", out)
 				break
 			}
 		case CommandEditor:
 			{
-				if err := OpenNeovim(playgroundFiles); err != nil {
+				if err := OpenNeovim(playground); err != nil {
 					return err
 				}
 				break
@@ -104,120 +96,27 @@ func CommandLoop(playgroundFiles *PlaygroundFiles) error {
 	}
 }
 
-func OpenNeovim(playgroundFiles *PlaygroundFiles) error {
-	relativeFilename := playgroundFiles.RelativeFilename()
+func OpenNeovim(playground *Playground) error {
+	relativeFilename := playground.GetRelativeFilename()
 
 	cmd := exec.Command("nvim", relativeFilename)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Dir = playgroundFiles.DirName
+	cmd.Dir = playground.DirName
 
 	err := cmd.Run()
 
 	if err != nil {
 		return handleExecError(
 			err,
-			fmt.Sprintf("Failed to open neovim in %s", playgroundFiles.File.Name()),
+			fmt.Sprintf("Failed to open neovim in %s", playground.File.Name()),
 		)
 	}
 
 	return nil
 }
 
-func RunScript(playgroundFiles *PlaygroundFiles) string {
+func RunScript(playground *Playground) string {
 	return "TODO: implement running script\n\n"
-}
-
-func SetupPlaygroundConfig(dirname string) error {
-	// create tsconfig
-	{
-		cmd := exec.Command("npx", "-y", "tsc", "--init")
-		cmd.Dir = dirname
-
-		if out, err := cmd.Output(); err != nil {
-			fmt.Println(out)
-			return handleExecError(err, "Failed to setup playground project")
-		}
-	}
-
-	return nil
-}
-
-type PlaygroundFiles struct {
-	File    *os.File
-	DirName string
-}
-
-func (pf *PlaygroundFiles) RelativeFilename() string {
-	if pf.File == nil {
-		return ""
-	}
-
-	_, filename := path.Split(pf.File.Name())
-	return filename
-}
-
-func CleanupFiles(playgroundFiles *PlaygroundFiles) error {
-	var err error
-	if playgroundFiles.File != nil {
-		_ = playgroundFiles.File.Close()
-	}
-
-	if playgroundFiles.DirName != "" {
-		err = os.RemoveAll(playgroundFiles.DirName)
-	}
-
-	return err
-}
-
-func getTempFile() (*PlaygroundFiles, error) {
-	osTempDir := os.TempDir()
-	if osTempDir == "" {
-		return nil, errors.New("failed to retrieve temporary directory")
-	}
-
-	out := PlaygroundFiles{}
-
-	var err error
-	out.DirName, err = os.MkdirTemp(osTempDir, "ts-playground-*")
-	if err != nil {
-		return nil, errors.Join(
-			errors.New("failed to create temporary directory"),
-			err,
-		)
-	}
-
-	tempfileName := path.Join(out.DirName, "playground.ts")
-	out.File, err = os.Create(tempfileName)
-	if err != nil {
-		CleanupFiles(&out)
-		return nil, errors.Join(
-			errors.New("failed to create temporary file"),
-			err,
-		)
-	}
-
-	err = os.Chmod(out.File.Name(), os.ModePerm)
-	if err != nil {
-		CleanupFiles(&out)
-		return nil, errors.Join(
-			errors.New("failed to get allocated permission on playground file"),
-			err,
-		)
-	}
-
-	return &out, nil
-}
-
-// log stderr and wrap the error messaging
-func handleExecError(err error, message string) error {
-	if exitErr, isExitErr := err.(*exec.ExitError); isExitErr {
-		fmt.Fprint(os.Stderr, string(exitErr.Stderr))
-	}
-
-	return errors.Join(
-		errors.New(message),
-		err,
-	)
 }
---
+++ playground.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func CreatePlayground() (*Playground, error) {
+	playground, err := setupTempFiles()
+	if err != nil {
+		return playground, err
+	}
+
+	fmt.Printf("Created temp file at %s\n", playground.File.Name())
+
+	err = setupPlaygroundConfig(playground.DirName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return playground, err
+	}
+
+	fmt.Printf("Setup playground at %s\n", playground.DirName)
+
+	return playground, nil
+}
+
+type Playground struct {
+	File    *os.File
+	DirName string
+}
+
+func (playground *Playground) GetRelativeFilename() string {
+	if playground.File == nil {
+		return ""
+	}
+
+	_, filename := path.Split(playground.File.Name())
+	return filename
+}
+
+func (playground *Playground) Destroy() error {
+	if playground == nil {
+		return nil
+	}
+
+	var err error
+	if playground.File != nil {
+		_ = playground.File.Close()
+	}
+
+	if playground.DirName != "" {
+		err = os.RemoveAll(playground.DirName)
+	}
+
+	return err
+}
+
+func setupPlaygroundConfig(dirname string) error {
+	// create tsconfig
+	{
+		cmd := exec.Command("npx", "-y", "tsc", "--init")
+		cmd.Dir = dirname
+
+		if out, err := cmd.Output(); err != nil {
+			fmt.Println(string(out))
+			return handleExecError(err, "Failed to setup playground project")
+		}
+	}
+
+	return nil
+}
+
+func setupTempFiles() (*Playground, error) {
+	osTempDir := os.TempDir()
+	if osTempDir == "" {
+		return nil, errors.New("failed to retrieve temporary directory")
+	}
+
+	out := Playground{}
+
+	var err error
+	out.DirName, err = os.MkdirTemp(osTempDir, "ts-playground-*")
+	if err != nil {
+		return nil, errors.Join(
+			errors.New("failed to create temporary directory"),
+			err,
+		)
+	}
+
+	tempfileName := path.Join(out.DirName, "playground.ts")
+	out.File, err = os.Create(tempfileName)
+	if err != nil {
+		out.Destroy()
+		return nil, errors.Join(
+			errors.New("failed to create temporary file"),
+			err,
+		)
+	}
+
+	err = os.Chmod(out.File.Name(), os.ModePerm)
+	if err != nil {
+		out.Destroy()
+		return nil, errors.Join(
+			errors.New("failed to get allocated permission on playground file"),
+			err,
+		)
+	}
+
+	return &out, nil
+}
+
+// log stderr and wrap the error messaging
+func handleExecError(err error, message string) error {
+	if exitErr, isExitErr := err.(*exec.ExitError); isExitErr {
+		fmt.Fprint(os.Stderr, string(exitErr.Stderr))
+	}
+
+	return errors.Join(
+		errors.New(message),
+		err,
+	)
+}
